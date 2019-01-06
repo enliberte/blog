e57bd1d2ee768bcc5981780eb67bda32b5c377fa
enliberte <zugife21@gmail.com>
2019-01-06 15:29:58 +0300

    'initial'

---
+++ app/blogengine/blog/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
+# Create your models here.
+
+
+def gen_slug(title):
+    slug = slugify(title, allow_unicode=True)
+    return '%s-%s' % (slug, int(time()))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '%s' % self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-date_pub']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '%s' % self.title
+
+    class Meta:
+        ordering = ['title']
